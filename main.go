dd8cc7ba2d1620bff8b7639021d07ca89ffe5ea0
hubert <hubert@hubert.com>
2025-03-10 07:07:24 +0100

    implement shell command execution based on OS type

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func getNixShell() string {
+	shell := os.Getenv("SHELL")
+	if shell == "" {
+		shell = "/bin/sh"
+	}
+	return shell
+}
+
+func getWinShell() string {
+	shell := os.Getenv("ComSpec")
+	if shell == "" {
+		shell = "cmd"
+	}
+	return shell
+}
+
+func getShell() string {
+	switch runtime.GOOS {
+	case "linux":
+		return getNixShell()
+	case "openbsd":
+		return getNixShell()
+	case "freebsd":
+		return getNixShell()
+	case "darwin":
+		return getNixShell()
+	case "windows":
+		return getWinShell()
+	}
+
+	panic(("Undefined GOOS: " + runtime.GOOS))
+}
+
 func main() {
-	println("Hello Nixman!")
+	shell := getShell()
+	command := exec.Command(shell)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	err := command.Run()
+	if err != nil {
+		panic(err)
+	}
 }
